f962f3329803c8573460d1710135c4e4ceb65e7c
manystoring <izoevislamizoevislam@gmail.com>
2025-03-31 16:54:23 +0200

    yooo, leaked

---
+++ s/mod/footer.php
@@ -0,0 +1,115 @@
+<!-- begin footer -->
+      <div id="footer">
+      <div class="yt-horizontal-rule "><span class="first"></span><span class="second"></span><span class="third"></span></div>
+
+    <div id="footer-logo">
+      <a href="/" title="SilasTube home">
+        <img src="http://s.ytimg.com/yt/img/pixel-vfl3z5WfW.gif" alt="SilasTube home">
+      </a>
+        <span class="copyright" dir="ltr">&copy; 2023 SilasTube, LLC</span>
+
+      <span id="footer-divider"></span>
+    </div>
+    <div id="footer-main">
+        
+    <div id="in-product-help" class="yt-uix-clickcard">
+      <button type="button" id="help-button" onclick=";return false;" class="yt-uix-clickcard-target yt-uix-button-reverse yt-uix-button yt-uix-button-default" data-orientation="vertical" data-locale="en_US" data-iph-anchor-text="More Help" data-iph-search-button-text="Search" data-iph-tracking="iph-questionmark" data-iph-js-url="//s.ytimg.com/yt/jsbin/www-help-vflyZa3c5.js" data-iph-search-input-text="Search SilasTube's Help Center" data-iph-title-text="Need Help on this page?" data-iph-topic-id="1699306" data-iph-css-url="//s.ytimg.com/yt/cssbin/www-helpie-vflhT2yf3.css" data-help-center-host="//support.google.com/youtube" role="button"><span class="yt-uix-button-content">  <img class="questionmark" src="http://s.ytimg.com/yt/img/pixel-vfl3z5WfW.gif">
+  <span>Help</span>
+  <img class="yt-uix-button-arrow" src="http://s.ytimg.com/yt/img/pixel-vfl3z5WfW.gif">
+ </span></button>
+      <div class="yt-uix-clickcard-content" id="help-target">  <p class="loading-spinner">
+    <img src="http://s.ytimg.com/yt/img/pixel-vfl3z5WfW.gif" alt="">
+Loading...
+  </p>
+</div>
+    </div>
+
+      <ul id="footer-links-primary">
+        <li><a href="/t/about_silastube">About</a></li>
+        <li><a href="/t/press">Press &amp; Blogs</a></li>
+        <li><a href="/t/copyright_center">Copyright</a></li>
+        <li><a href="/t/partners">Creators &amp; Partners</a></li>
+        <li><a href="/t/advertising_overview">Advertising</a></li>
+        <li><a href="/dev">Developers</a></li>
+      </ul>
+
+
+      <ul id="footer-links-secondary">
+        <li><a href="/t/terms">Terms</a></li>
+        <li><a href="http://www.google.com/intl/en/policies/privacy/">Privacy</a></li>
+        <li><a href="http://support.google.com/youtube/bin/request.py?contact_type=abuse&amp;hl=en-US">Safety</a></li>
+        <li><a href="http://www.google.com/tools/feedback/intl/en/error.html" onclick="return yt.www.feedback.start(yt.getConfig('FEEDBACK_LOCALE_LANGUAGE'), yt.getConfig('FEEDBACK_LOCALE_EXTRAS'));" id="reportbug">Report a bug</a></li>
+        <li><a href="t/testtube">Try something new!</a></li>
+        <li><a href="https://discord.gg/jvMqJ8wffZ">Discord</a></li>
+        <li><a href="https://silastube.yooco.org">SilasTube Classic!</a></li>
+        <li><a href="https://silastube.yooco.org/games">Games</a></li>
+      </ul>
+        <ul class="pickers yt-uix-button-group" data-button-toggle-group="optional">
+      <li>
+Language:
+          
+  <button type="button" class=" yt-uix-button yt-uix-button-text" onclick=";return false;" data-button-toggle="true" data-picker-position="footer" data-button-menu-id="arrow-display" data-picker-key="language" data-button-action="yt.www.picker.load" role="button"><span class="yt-uix-button-content">English </span><img class="yt-uix-button-arrow" src="http://s.ytimg.com/yt/img/pixel-vfl3z5WfW.gif" alt=""></button>
+
+
+      </li>
+      <li>
+Location:
+          
+  <button type="button" class=" yt-uix-button yt-uix-button-text" onclick=";return false;" data-button-toggle="true" data-picker-position="footer" data-button-menu-id="arrow-display" data-picker-key="country" data-button-action="yt.www.picker.load" role="button"><span class="yt-uix-button-content">Worldwide </span><img class="yt-uix-button-arrow" src="http://s.ytimg.com/yt/img/pixel-vfl3z5WfW.gif" alt=""></button>
+
+
+      </li>
+      <li>
+Safety:
+          
+  <button type="button" class=" yt-uix-button yt-uix-button-text" onclick=";return false;" data-button-toggle="true" data-picker-position="footer" data-button-menu-id="arrow-display" data-picker-key="safetymode" data-button-action="yt.www.picker.load" role="button"><span class="yt-uix-button-content">Off
+ </span><img class="yt-uix-button-arrow" src="http://s.ytimg.com/yt/img/pixel-vfl3z5WfW.gif" alt=""></button>
+
+
+      </li>
+  </ul>
+        <div id="yt-picker-language-footer" class="yt-picker" style="display: none">
+      <p class="yt-spinner">
+    <img src="http://s.ytimg.com/yt/img/pixel-vfl3z5WfW.gif" class="yt-spinner-img" alt="">
+Loading...
+  </p>
+
+  </div>
+
+      <div id="yt-picker-country-footer" class="yt-picker" style="display: none">
+      <p class="yt-spinner">
+    <img src="http://s.ytimg.com/yt/img/pixel-vfl3z5WfW.gif" class="yt-spinner-img" alt="">
+Loading...
+  </p>
+
+  </div>
+
+      <div id="yt-picker-safetymode-footer" class="yt-picker" style="display: none">
+      <p class="yt-spinner">
+    <img src="http://s.ytimg.com/yt/img/pixel-vfl3z5WfW.gif" class="yt-spinner-img" alt="">
+Loading...
+  </p>
+
+  </div>
+
+
+
+
+          
+
+
+    </div>
+  </div>
+
+<!-- end footer -->
+
+
+
+
+
+
+
+
+
+
+
